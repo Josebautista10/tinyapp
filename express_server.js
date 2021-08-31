4358656e0a985b80e5de28d9f22621d95167e3fb
Jose Bautista <Cooldj1020@gmail.com>
2021-08-31 17:23:58 -0600

    added a edit and delete feature

--- express_server.js
+++ express_server.js
@@ -13,6 +13,7 @@ const generateRandomString = () => {
   return result;
 };
 
+// in-memory database
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
@@ -21,17 +22,20 @@ const urlDatabase = {
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//generates a key of random characters and redirects to '/urls/:shortURL'
 app.post('/urls', (req, res) => {
   const id = generateRandomString();
   urlDatabase[id] = req.body.longURL;
   res.redirect(`/urls/${id}`);
 });
 
+// browse GET urls
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+// browse GET new urls
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
@@ -41,7 +45,6 @@ app.get('/urls/:shortURL', (req, res) => {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
-  console.log(req.params.shortURL);
   res.render('urls_show', templateVars);
 });
 
@@ -54,6 +57,18 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL/update', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.newURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL/edit', (req, res) => {
+  const shortURL = req.params.shortURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/', (req, res) => {
